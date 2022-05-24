1374945d927a7e5a74448626ff5dfa1798bddaee
ARez <75789249+ARez2@users.noreply.github.com>
2022-05-24 17:58:40 +0200

    Added comments to LeaderboardsResponse.gd

--- src/LootLockerSDK/leaderboards/LeaderboardsResponse.gd
+++ src/LootLockerSDK/leaderboards/LeaderboardsResponse.gd
@@ -1,5 +1,6 @@
 class_name LootLockerLeaderboardsResponse
+# Just holds the result and pagination classes. Useful for making sure our SDK
+# always returns the same stuff
 
 var result : Array[LootLockerLeaderboardResult] = []
 var pagination : LootLockerLeaderboardPagination = null
-
