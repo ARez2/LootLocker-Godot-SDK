7dd4314e48f9f1cb7e5a1b37519190b768940f2d
ARez <75789249+ARez2@users.noreply.github.com>
2022-05-24 18:00:43 +0200

    Added comments to LeaderboardResult.gd

--- src/LootLockerSDK/leaderboards/LeaderboardResult.gd
+++ src/LootLockerSDK/leaderboards/LeaderboardResult.gd
@@ -1,4 +1,7 @@
 class_name LootLockerLeaderboardResult
+# Wrapper class to store the placement data returned by the API
+# If requesting data from a Leaderboard of type 'player' (NOT 'generic') the player
+# Variable is filled with a LootLockerUser wrapper-class (includes name, id, currency etc.)
 
 var member_id : int = 0
 var rank : int = 0
