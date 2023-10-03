c9d4a525c1f25c342f5c863f79f4571a67decb8e
Infini-Creation <infini.creation.france@gmail.com>
2023-10-03 22:05:12 +0200

    Update LootLockerUserDataVault.gd
    -API_KEY renamed GAME_API_KEY
    -add DEV_MODE & SYNCHRONOUS item
    -prevent error in case items are missing on loading
    -adjust style of some lines

--- src/LootLockerSDK/LootLockerUserDataVault.gd
+++ src/LootLockerSDK/LootLockerUserDataVault.gd
@@ -7,16 +7,17 @@ const SDKDATA_FILEPATH : String = "user://LootLockerSDK.cfg"
 const PASSWORD_FILEPATH : String = "user://.sdk_data_password"
 
 const DEFAULT_PASSWORD : String = "NOTSETYET"
-const DEFAULT_API_KEY : String = "DEFAULT_API_KEY"
+const DEFAULT_GAME_API_KEY : String = "DEFAULT_GAME_API_KEY"
 const DEFAULT_SERVER_API_KEY : String = "DEFAULT_SERVER_API_KEY"
 const DEFAULT_DOMAIN_KEY : String = "DEFAULT_DOMAIN_KEY"
 const DEFAULT_GAME_VERSION : String = "0.0.0.0"
 
-static var API_KEY : String = DEFAULT_API_KEY
-static var SERVER_API_KEY : String = "DEFAULT_SERVER_KEY"
-static var DOMAIN_KEY : String = "DEFAULT_DOMAIN_KEY"
-static var GAME_VERSION : String = "GAME_VERSION"
-
+static var GAME_API_KEY : String = DEFAULT_GAME_API_KEY
+static var SERVER_API_KEY : String = DEFAULT_SERVER_API_KEY
+static var DOMAIN_KEY : String = DEFAULT_DOMAIN_KEY
+static var GAME_VERSION : String = DEFAULT_GAME_VERSION
+static var DEV_MODE : bool = true
+static var SYNCHRONOUS_MODE : bool = true
 static var password : String = DEFAULT_PASSWORD
 
 
@@ -58,12 +59,14 @@ static func save_sdk_data() -> int:
 	
 	var file = FileAccess.open_encrypted_with_pass(SDKDATA_FILEPATH, FileAccess.WRITE, password)
 	if file != null:
-		file.store_pascal_string(API_KEY)
+		file.store_pascal_string(GAME_API_KEY)
 		if file.get_error() != OK:
 			print("[check_password]: error while reading password data: "+str(file.get_error()))
 		file.store_pascal_string(SERVER_API_KEY)
 		file.store_pascal_string(DOMAIN_KEY)
 		file.store_pascal_string(GAME_VERSION)
+		file.store_var(DEV_MODE)
+		file.store_var(SYNCHRONOUS_MODE)
 		status = OK
 	else:
 		push_error("[save_sdk_data] Error: "+str(FileAccess.get_open_error()))
@@ -72,37 +75,56 @@ static func save_sdk_data() -> int:
 
 
 static func load_sdk_data() -> int:
-	var status : int = FAILED
-	
+	var status : int = OK
+	var temp
+
 	if check_password() != OK:
 		print("[load_sdk_data]: main password is invalid !")
-		return status
+		return FAILED
 
 	print("try to open file "+SDKDATA_FILEPATH)
 	var file = FileAccess.open_encrypted_with_pass(SDKDATA_FILEPATH, FileAccess.READ, password)
 	print("file="+str(file))
 	if file != null:
-		API_KEY = file.get_pascal_string ()
-		print("APIK="+API_KEY)
+		GAME_API_KEY = file.get_pascal_string()
+		print("GAPIK="+GAME_API_KEY)
 		if file.get_error() != OK:
 			print("[load_sdk_data]: error while reading API_KEY data: "+str(file.get_error()))
-		if API_KEY == DEFAULT_API_KEY:
-			print("[load_sdk_data]: API_KEY has not been configured yet !")
+			status = file.get_error()
+		if GAME_API_KEY == DEFAULT_GAME_API_KEY:
+			print("[load_sdk_data]: GAME_API_KEY has not been configured yet !")
 		else:
-			print("[load_sdk_data]: API_KEY="+API_KEY)
+			print("[load_sdk_data]: GAME_API_KEY="+GAME_API_KEY)
 
-		SERVER_API_KEY = file.get_pascal_string ()
+		SERVER_API_KEY = file.get_pascal_string()
 		print("SAPIK="+SERVER_API_KEY)
 		if file.get_error() != OK:
 			print("[load_sdk_data]: error while reading SERVER_API_KEY data: "+str(file.get_error()))
-		DOMAIN_KEY = file.get_pascal_string ()
+			status = file.get_error()
+		DOMAIN_KEY = file.get_pascal_string()
 		if file.get_error() != OK:
 			print("[load_sdk_data]: error while reading DOMAIN_KEY data: "+str(file.get_error()))
-		GAME_VERSION = file.get_pascal_string ()
+			status = file.get_error()
+		GAME_VERSION = file.get_pascal_string()
 		if file.get_error() != OK:
 			print("[load_sdk_data]: error while reading GAME_VERSION data: "+str(file.get_error()))
-		status = OK
+			status = file.get_error()
+		temp = file.get_var()
+		if file.get_error() != OK:
+			print("[load_sdk_data]: error while reading DEV_MODE data: "+str(file.get_error()))
+			status = file.get_error()
+		else:
+			if temp != null and typeof(temp) == TYPE_BOOL:
+				DEV_MODE = temp
+		temp = file.get_var()
+		if file.get_error() != OK:
+			print("[load_sdk_data]: error while reading SYNCHRONOUS_MODE data: "+str(file.get_error()))
+			status = file.get_error()
+		else:
+			if temp != null and typeof(temp) == TYPE_BOOL:
+				SYNCHRONOUS_MODE = temp
 	else:
 		push_error("[load_sdk_data] Error: "+str(FileAccess.get_open_error()))
+		status = FAILED
 
 	return status
